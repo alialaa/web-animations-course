aa7135edeb5a91363375484d2ca7d9ae98ee8a6a
alialaa <hello@alialaa.com>
2024-02-19 12:37:20 +0200

    Code for waapi-iteration-composite

--- web-api/script.js
+++ web-api/script.js
@@ -1,39 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.querySelector(".square");
   const squareAnimation = element.animate(
-    {
-      transform: [
-        "translateX(0)",
-        "translateX(calc(100vw - 100px)) rotate(360deg)",
-      ], // offset [0,1]
-      backgroundColor: ["gold", "blue", "crimson"], // offset [0,0.5,1]
-      offset: [0, 0.3, 1], // or [0, 0.3]
-      easing: ["ease-in", "linear"],
-      composite: ["add", "replace", "add"],
-    },
-    // [
-    //   {
-    //     transform: "translateX(0)",
-    //     easing: "ease-in",
-    //   },
-    //   {
-    //     backgroundColor: "blue",
-    //     offset: 0.8,
-    //     composite: "replace",
-    //   },
-    //   {
-    //     transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
-    //     backgroundColor: "crimson",
-    //   },
-    // ],
+    [
+      {
+        transform: "translateX(0)",
+        easing: "ease-in",
+      },
+      {
+        backgroundColor: "blue",
+        offset: 0.8,
+        composite: "replace",
+      },
+      {
+        transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
+        backgroundColor: "crimson",
+      },
+    ],
     {
       duration: 3000,
       delay: 1000,
-      direction: "alternate",
+      direction: "normal",
       fill: "both",
       iterations: Infinity,
       easing: "linear",
       composite: "add",
+      iterationComposite: "accumulate",
       timeline: document.timeline,
     }
   );
