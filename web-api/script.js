2566c0eda77a21974ebae50b1c73a9f3864a1175
alialaa <hello@alialaa.com>
2024-02-19 11:46:40 +0200

    Code for waapi-first-animation

--- web-api/script.js
+++ web-api/script.js
@@ -1,3 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.querySelector(".square");
+  const squareAnimation = element.animate(
+    [
+      {
+        transform: "translateX(0)",
+      },
+      {
+        backgroundColor: "blue",
+        offset: 0.8,
+      },
+      {
+        transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
+        backgroundColor: "crimson",
+      },
+    ],
+    {
+      duration: 3000,
+      delay: 1000,
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "linear",
+      composite: "add",
+      timeline: document.timeline,
+    }
+  );
+  squareAnimation.pause();
+  // const squareAnimationKeyframes = new KeyframeEffect(
+  //   element,
+  //   [
+  //     {
+  //       transform: "translateX(0)",
+  //     },
+  //     {
+  //       backgroundColor: "blue",
+  //       offset: 0.8,
+  //     },
+  //     {
+  //       transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
+  //       backgroundColor: "crimson",
+  //     },
+  //   ],
+  //   {
+  //     duration: 3000,
+  //     delay: 1000,
+  //     direction: "alternate",
+  //     fill: "both",
+  //     iterations: Infinity,
+  //     easing: "linear",
+  //     composite: "add",
+  //   }
+  // );
+  // const squareAnimation = new Animation(
+  //   squareAnimationKeyframes,
+  //   document.timeline
+  // );
+  // squareAnimation.play();
 });
