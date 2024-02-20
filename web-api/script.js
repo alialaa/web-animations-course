61db6fe9f102b11433eeb97352b442a6c0ad81a7
alialaa <hello@alialaa.com>
2024-02-20 10:41:38 +0200

    Code for waapi-animation-object-methods

--- web-api/script.js
+++ web-api/script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       duration: 3000,
       delay: 1000,
-      direction: "normal",
+      direction: "alternate",
       fill: "both",
       iterations: Infinity,
       easing: "linear",
@@ -28,5 +28,36 @@ document.addEventListener("DOMContentLoaded", () => {
       timeline: document.timeline,
     }
   );
-  // squareAnimation.pause();
+  squareAnimation.pause();
+
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.classList.contains("play")) {
+        squareAnimation.play();
+      }
+      if (button.classList.contains("pause")) {
+        squareAnimation.pause();
+      }
+      if (button.classList.contains("cancel")) {
+        squareAnimation.cancel();
+      }
+      if (button.classList.contains("reverse")) {
+        squareAnimation.reverse();
+      }
+      if (button.classList.contains("finish")) {
+        squareAnimation.finish();
+      }
+    });
+  });
+
+  const playbackRateInput = document.getElementById("playbackRateInput");
+  const playbackRateInputValue = document.getElementById(
+    "playbackRateInputValue"
+  );
+
+  playbackRateInput.addEventListener("input", (e) => {
+    squareAnimation.updatePlaybackRate(e.target.value);
+    playbackRateInputValue.value = e.target.value;
+  });
 });
