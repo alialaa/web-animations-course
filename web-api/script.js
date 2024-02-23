1b1fed475d2e2c410faa3c551e5a8551385a59f3
alialaa <hello@alialaa.com>
2024-02-23 14:55:41 +0200

    Code for waapi-automatic-animation-removal

--- web-api/script.js
+++ web-api/script.js
@@ -1,32 +1,50 @@
 document.addEventListener("DOMContentLoaded", async () => {
   const element = document.querySelector(".square");
-
-  const squareAnimation = element.animate(
-    [
+  const animations = [];
+  const addNewAnimation = () => {
+    const squareAnimation = element.animate(
+      [
+        {
+          transform: "translateX(0)",
+          easing: "ease-in",
+        },
+        {
+          backgroundColor: "blue",
+          offset: 0.8,
+          composite: "replace",
+        },
+        {
+          transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
+          backgroundColor: "crimson",
+        },
+      ],
       {
-        transform: "translateX(0)",
-        easing: "ease-in",
-      },
-      {
-        backgroundColor: "blue",
-        offset: 0.8,
+        duration: 3000,
+        // delay: 1000,
+        direction: "alternate",
+        fill: "both",
+        iterations: 1,
+        easing: "linear",
         composite: "replace",
-      },
-      {
-        transform: "translateX(calc(100vw - 100px)) rotate(360deg)",
-        backgroundColor: "crimson",
-      },
-    ],
-    {
-      duration: 3000,
-      delay: 1000,
-      direction: "alternate",
-      fill: "both",
-      iterations: 1,
-      easing: "linear",
-      composite: "replace",
-      iterationComposite: "accumulate",
-      timeline: document.timeline,
-    }
-  );
+        iterationComposite: "accumulate",
+        timeline: document.timeline,
+      }
+    );
+    squareAnimation.persist();
+    squareAnimation.addEventListener("remove", (e) => {
+      console.log("Animation removed by the browser", e);
+    });
+    animations.push(squareAnimation);
+  };
+
+  const addAnimationButton = document.getElementById("add-animation-button");
+  addAnimationButton.addEventListener("click", () => {
+    addNewAnimation();
+  });
+
+  const logAnimationButton = document.getElementById("log-animations-button");
+  logAnimationButton.addEventListener("click", () => {
+    console.log(animations);
+    console.log(element.getAnimations());
+  });
 });
