3783f2a3e86746c22686061dfaae3595827fe8c8
alialaa <hello@alialaa.com>
2024-02-24 11:22:05 +0200

    Code for exercise-2-running-and-jump

--- exercise-2/script.js
+++ exercise-2/script.js
@@ -1,1 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const character = document.querySelector(".character");
+  const characterAnimation = character.animate(
+    [
+      {
+        backgroundPosition: "0 0",
+      },
+      {
+        backgroundPosition: "calc(var(--char-width) * -7) 0",
+      },
+    ],
+    {
+      duration: 1000,
+      iterations: Infinity,
+      easing: "steps(8, jump-none)",
+    }
+  );
+
+  async function jump() {
+    if (character.getAnimations().find((animation) => animation.id === "jump"))
+      return;
+    characterAnimation.pause();
+    character.classList.add("jump");
+    const jumpAnimation = character.animate(
+      [
+        {
+          transform: "translateY(0)",
+        },
+        {
+          transform: "translateY(-70px)",
+        },
+      ],
+      {
+        id: "jump",
+        duration: 500,
+        iterations: 2,
+        direction: "alternate",
+        easing: "ease-in-out",
+      }
+    );
+    await jumpAnimation.finished;
+    characterAnimation.play();
+    character.classList.remove("jump");
+  }
+
+  document.addEventListener("keyup", (event) => {
+    switch (event.code) {
+      case "ArrowUp":
+        jump();
+        break;
+      case "ArrowRight":
+        break;
+      case "ArrowLeft":
+        break;
+      case "Space":
+        break;
+
+      default:
+        break;
+    }
+  });
+});
