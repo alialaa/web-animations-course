89b595d4874cd830697fa182c018ab20b8341636
alialaa <hello@alialaa.com>
2024-03-25 12:03:18 +0200

    Lecture Code For ex-3-logo-animation

--- exercise-3/script.js
+++ exercise-3/script.js
@@ -1,1 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const logoSVG = document.getElementById("logo");
+  const logoSVGPath = document.querySelector(".cls-1");
+  logoSVGPath.addEventListener("animationend", () => {
+    logoSVG.classList.remove("animate");
+    setTimeout(() => {
+      logoSVG.classList.add("animate");
+    }, 5000);
+  });
+});
