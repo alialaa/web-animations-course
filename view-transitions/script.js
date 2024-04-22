98e6ccda4483740c62323c8ab429a00ed632c0e4
alialaa <hello@alialaa.com>
2024-04-22 09:33:27 +0200

    Lecture Code For view-transitions-init

---
+++ view-transitions/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const header = document.querySelector(".header");
+  const gridOuter = document.querySelector(".grid-outer");
+  const main = document.querySelector(".main .item");
+  const gridButton = document.querySelector(".grid-view-button");
+
+  function expandImage(item) {
+    const title = item.querySelector("h3").innerText;
+    const largeImage = item.dataset.largeImage;
+    main.querySelector("h2").innerText = title;
+    main.querySelector("img").src = largeImage;
+    gridButton.style.display = "block";
+    header.classList.add("expanded");
+    gridOuter.classList.add("expanded");
+
+    grid
+      .querySelectorAll(".active")
+      .forEach((e) => e.classList.remove("active"));
+    item.classList.add("active");
+  }
+
+  function displayGrid() {
+    document.documentElement.scrollTop = 0;
+    gridButton.style.display = "none";
+    gridOuter.classList.remove("expanded");
+    header.classList.remove("expanded");
+    grid
+      .querySelectorAll(".active")
+      .forEach((e) => e.classList.remove("active"));
+  }
+
+  grid.addEventListener("click", async (e) => {
+    const item = e.target.closest(".grid-item");
+    if (!item || item.classList.contains("active")) return;
+    expandImage(item);
+  });
+  
+  gridButton.addEventListener("click", async (e) => {
+    displayGrid();
+  });
+});
